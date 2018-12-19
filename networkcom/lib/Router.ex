9e097d63791d4e6461327647d092ea40c29b0c38
Evan Chopra <evanchopra@Evans-MacBook-Pro.local>
2018-12-19 12:28:37 -0800

    Active local netowrk com. Post request does not work.... wokring on a fix

--- networkcom/lib/Router.ex
+++ networkcom/lib/Router.ex
@@ -2,10 +2,22 @@
 #Basic Router module that can decide which messages to transmit base on HTTP link
 defmodule Networkcom.Router do
     use Plug.Router
+    import Jason
 
     plug(:match)
+     plug Plug.Parsers, parsers: [:json],
+                       pass: ["application/json"],
+                       json_decoder: Jason
     plug(:dispatch)
 
-    get("/ping", do: send_resp(conn, 200, "Pinged\n"))
-    match(_, do: send_resp(conn, 404, "Cannot communicate a message"))
+    post "/ping" do
+      IO.inspect("Someone is accessing the server")
+      IO.inspect conn.body_parems
+      send_resp(conn, 200, "Pinged\n")
+    end 
+    
+    match _ do
+        IO.inspect("Someone is communicating using an invalid HTTP")
+      send_resp(conn, 404, "Cannot communicate a message\n")
+    end 
 end
